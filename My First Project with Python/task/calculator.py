e85052c4f2e7251e9efc07e55250bbe5e5823fa2
Benjamin Ayesu <ben.ayesu.attah@gmail.com>
2023-12-04 18:49:06 -0800

    Completed course and answers

--- My First Project with Python/task/calculator.py
+++ My First Project with Python/task/calculator.py
@@ -1,10 +1,14 @@
-# Write your code here
-print("Prices:")
-print("Bubblegum: $2")
-print("Toffee: $0.2")
-print("Ice cream: $5")
-print("Milk chocolate: $4")
-print("Doughnut: $2.5")
-print("Pancake: $3.2")
+items_prices = {
+    'Bubblegum': 202,
+    'Toffee': 118,
+    'Ice cream': 2250,
+    'Milk chocolate': 1680,
+    'Doughnut': 1075,
+    'Pancake': 80
+}
 
 
+print('Earned amount:')
+for key, value in items_prices.items():
+    print(f'{key}: ${value}')
+print(f'Income ${sum(items_prices.values())}')
